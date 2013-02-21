7efaa0af99b86e0e8eb97f8071bd51990b36468c
mikedeboer <info@mikedeboer.nl>
2013-02-21 23:34:40 +0100

    reformatted jsd parser and fixed jshint warnings/ errors

--- tests/check_test.js
+++ tests/check_test.js
@@ -1,25 +1,25 @@
-var panino = require("../index.js");
-var wrench = require("wrench");
-var path = require("path");
+"use strict";
+
+var Panino = require("../index.js");
 
 var options = {
-  title       : "TEST",
-  output      : "./tests/test_out",
-  additionalObjs : "./additionalObjs.json"
+    title: "TEST",
+    output: "./tests/test_out",
+    additionalObjs: "./additionalObjs.json"
 };
 
-panino.parse(["./tests/features/new_tags.js"], options, function (err, ast) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-
-  panino.render('json', ast, options, function (err) {
+Panino.parse(["./tests/features/new_tags.js"], options, function(err, ast) {
     if (err) {
-      console.error(err);
-      process.exit(1);
+        console.error(err);
+        process.exit(1);
     }
-  });
+
+    Panino.render("json", ast, options, function(err) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    });
 });
 
 //Panino.build(["./nodejs_ref_guide", "-e", "markdown", "-f", "html", "-d", "-p", "./nodeParseOptions.json", "-a", "./additionalObjs.json", "-o", "./out/", "-t", "Prototype Docs", "--skin", "./skins/goose/", "-s", "-r"]);
